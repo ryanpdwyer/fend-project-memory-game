2752cc324ea9c0d736611ae2d3e9b60657548107
Ryan Dwyer <ryanpdwyer@gmail.com>
2018-01-16 12:25:11 -0500

    feat: Concentration game logic
    Added matching logic, star scoring system, move tracker, and restart button logic.

--- js/app.js
+++ js/app.js
@@ -2,37 +2,155 @@
  * Create a list that holds all of your cards
  */
 
+const li = document.getElementById("card-list");
+const scorePanel = document.getElementById("score-panel");
+const cards = li.children;
+const restart = document.querySelector(".restart");
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+let gameState = {
+    clicksInCurrentMove: 0,
+    moves: 0,
+    cardMatch: [],
+    matchesRemaining: 8,
+ };
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+function initializeGame() {
+    /* Re-shuffle the cards. HTMLCollection must be converted to an array
+     * in order for the shuffle function to work.
+     */
+    const shuffled_cards = shuffle(Array.from(cards));
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+    // Remove all 'open', 'show', and 'match' classes to cards back over.
+    // Then append the card elements to update the position of the 
+    // shuffled cards
+    for (card of shuffled_cards) {
+        card.classList.remove("open");
+        card.classList.remove("show");
+        card.classList.remove("match");
+        li.append(card);
     }
 
-    return array;
+    // Reset game state to initial value
+    gameState.clicksInCurrentMove = 0;
+    gameState.moves = 0;
+    gameState.cardMatch = [];
+    gameState.matchesRemaining = 8;
+
+    // Reset stars and moves counter.
+    const starList = scorePanel.firstElementChild
+    for (let starItem of starList.children) {
+        const star = starItem.firstElementChild;
+        star.classList.add("fa-star");
+        star.classList.remove("fa-star-o");
+    }
+    updateScore();
+}
+
+initializeGame();
+
+
+function resolveGameState() {
+        gameState.moves += 1;
+        gameState.clicksInCurrentMove = 0;
+        card1 = gameState.cardMatch.shift();
+        card2 = gameState.cardMatch.shift();
+        // Matching condition
+        if (card1.firstElementChild.classList[1] == card2.firstElementChild.classList[1]) {
+            // If match, restyle elements and subtract a match!
+            card1.classList.add("match")
+            card2.classList.add("match")
+
+            gameState.matchesRemaining -= 1;
+        }
+
+        updateScore();
+        // Either way, remove open and show classes.
+        card1.classList.remove("open");
+        card1.classList.remove("show");
+        card2.classList.remove("open");
+        card2.classList.remove("show");
+
+        if (gameState.matchesRemaining == 0) {
+            alert(`You won in ${gameState.moves} moves!`);
+        }
+}
+
+function updateScore() {
+    const starList = scorePanel.firstElementChild;
+    const movesSpan = scorePanel.children[1];
+    movesSpan.textContent = `${gameState.moves}`;
+    if ((gameState.moves + gameState.matchesRemaining) == 13) {
+        const star = starList.children[2].firstElementChild;
+        star.classList.remove("fa-star");
+        star.classList.add("fa-star-o");
+    } else if ((gameState.moves + gameState.matchesRemaining) == 15) {
+        const star = starList.children[1].firstElementChild;
+        star.classList.remove("fa-star");
+        star.classList.add("fa-star-o");
+    } else if ((gameState.moves + gameState.matchesRemaining) == 18) {
+        const star = starList.children[0].firstElementChild;
+        star.classList.remove("fa-star");
+        star.classList.add("fa-star-o");
+    }
+}
+
+function clickBoard(event) {
+    let target = event.target;
+    if ((target.tagName == "LI") && (target.classList.length == 1) && (gameState.clicksInCurrentMove < 2)) {
+        target.classList.add("open");
+        target.classList.add("show");
+        gameState.clicksInCurrentMove += 1;
+        gameState.cardMatch.push(target);
+        // Wait for a while before resolving the game state
+        // so the user can see the cards.
+        if (gameState.clicksInCurrentMove == 2) {
+            setTimeout(resolveGameState, 1250);
+        }
+    }
 }
 
 
+li.addEventListener('click', clickBoard);
+restart.addEventListener('click', initializeGame);
+
+
+
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ * Inlined from shuffle-array
+ * https://github.com/pazguille/shuffle-array
+ *
+ * Randomize the order of the elements in a given array.
+ * @param {Array} arr - The given array.
+ * @param {Object} [options] - Optional configuration options.
+ * @param {Boolean} [options.copy] - Sets if should return a shuffled copy of the given array. By default it's a falsy value.
+ * @param {Function} [options.rng] - Specifies a custom random number generator.
+ * @returns {Array}
  */
+function shuffle(arr, options) {
+
+  if (!Array.isArray(arr)) {
+    throw new Error('shuffle expect an array as parameter.');
+  }
+
+  options = options || {};
+
+  var collection = arr,
+      len = arr.length,
+      rng = options.rng || Math.random,
+      random,
+      temp;
+
+  if (options.copy === true) {
+    collection = arr.slice();
+  }
+
+  while (len) {
+    random = Math.floor(rng() * len);
+    len -= 1;
+    temp = collection[len];
+    collection[len] = collection[random];
+    collection[random] = temp;
+  }
+
+  return collection;
+};
